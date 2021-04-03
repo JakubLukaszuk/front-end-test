7d157dd55718aad3fd5d88c5d00252e348f73073
JakubLukaszuk <krypton97@wp.pl>
2021-04-03 20:40:55 +0200

    Update character

--- book/book.js
+++ book/book.js
@@ -1,35 +1,9 @@
 window.Book = {
     init: function (frame, store) {
         // initialisation stuff here
-        console.log(this);
         this.Api.store = store;
-        const characterListDom = $("<ul class='character-list'></ul>");
-        const characterForm = $(`
-        <form>
-            <label for="name">Name</label>
-            <input type="text" id="name">
-
-            <label for="female">Species</label>
-            <input type="text" id="species">
-
-            <img id="picture"/>
-
-            <label for="other">Description</label>
-            <input type="text" id="description">
-
-            <input type="submit" value="Submit">
-        </form>
-        `);
-
-        frame.append(characterForm);
-
-        frame.append(characterListDom);
-        $(document).ready(() => {
-
-            this.Api.fetchCharacters().then(
-                characters => this.List.fillListWithCharacters(characterListDom, characters, store)
-            )
-        })
+        this.List.init(frame);
+        this.Form.init(frame);
     },
     Api: {
         store: null,
@@ -55,9 +29,29 @@ window.Book = {
                         return error
                     })
         },
+        updateCharacter: function (character) {
+            return this.Book.Api.store.updateCharacter(character).then(
+                    details => {
+                        return details;
+                    }
+                )
+                .catch(
+                    error => {
+                        return error
+                    })
+        }
     },
     List: {
-        fillListWithCharacters: function (characterListDom, characters, store) {
+        init: (frame) => {
+            const characterListDom = $("<ul class='character-list'></ul>");
+            frame.append(characterListDom);
+            $(document).ready(() => {
+                this.Book.Api.fetchCharacters().then(
+                    characters => this.Book.List.fillListWithCharacters(characterListDom, characters)
+                )
+            })
+        },
+        fillListWithCharacters: function (characterListDom, characters) {
             characters.sort((a, b) => a.name.localeCompare(b.name))
             characters.forEach(character => {
                 const characterListItem = $(`
@@ -73,13 +67,13 @@ window.Book = {
                 characterListItem.attr("data-isSelected", false)
                 const itemClick = this.onCharacterItemClick;
                 characterListItem.on("click", function () {
-                    itemClick(this, character, store);
+                    itemClick(this, character);
                 })
                 characterListDom.append(characterListItem);
             })
         },
 
-        onCharacterItemClick: function (domItem, character, store) {
+        onCharacterItemClick: function (domItem, character) {
             const clickedItem = $(domItem);
             $(".character-list").find(`[data-isSelected=true]`).data("data-isSelected", false).removeClass('character-list__item--selected')
             clickedItem.attr("data-isSelected", true);
@@ -88,16 +82,84 @@ window.Book = {
                 details => this.Book.Form.fillCharacterForm(details)
             )
         },
-
+        removeList: function () {
+            $(".character-list").remove();
+        }
     },
     Form: {
         selectedCharacter: null,
+        init: (frame) => {
+            const characterForm = $(`
+            <form>
+                <label for="name">Name</label>
+                <input type="text" id="name">
+
+                <label for="female">Species</label>
+                <input type="text" id="species">
+
+                <img id="picture"/>
+
+                <label for="other">Description</label>
+                <input type="text" id="description">
+
+            </form>
+            `);
+
+            const submitFormButton = $('<input type="submit" value="Submit"/>');
+
+            submitFormButton.on('click', (e) => {
+                e.preventDefault();
+                this.Book.Form.submitChanges(e, this.Book.Api.updateCharacter);
+                this.Book.List.removeList();
+                this.Book.List.init(frame);
+            })
+            characterForm.append(submitFormButton);
+            frame.append(characterForm);
+        },
+
         fillCharacterForm: function (details) {
             this.selectedCharacter = details
-            $("#name").attr('value', details.name)
+            $("#name").val(details.name)
             $("#picture").attr('src', details.picture)
-            $("#species").attr('value', details.species)
-            $("#description").attr('value', details.description)
+            $("#species").val( details.species)
+            $("#description").val( details.description)
+        },
+        getModifedValue: function () {
+            let isModifed= false;
+            const newName = $("#name").val();
+            const newSpecies = $("#species").val();
+            const newDescription = $("#description").val();
+
+            var newCharacterData = Object.assign({}, this.selectedCharacter);
+
+
+            if ( newName !== newCharacterData.name){
+                newCharacterData.name = newName;
+                isModifed= true;
+            }
+            if(newSpecies !== newCharacterData.species)
+            {
+                newCharacterData.species = species;
+                isModifed= true;
+            }
+            if(newDescription !== newCharacterData.description)
+            {
+                newCharacterData.description = description;
+                isModifed= true;
+            }
+            if(!isModifed)
+            {
+                return false
+            }
+            return newCharacterData
+        },
+        submitChanges: function (event, updateCharacter) {
+            event.preventDefault();
+            console.log(this);
+            const modifedValue = this.getModifedValue();
+            if (modifedValue) {
+                updateCharacter(modifedValue);
+            }
         }
     },
 
